3d58e628807627f28e68dfec1ad945f96d5f6511
Fabio Carrara <fabio.carrara@isti.cnr.it>
2019-04-01 13:17:18 +0200

    updated reproducibility script

--- reproduce.sh
+++ reproduce.sh
@@ -12,9 +12,21 @@ python train.py --model resnet --downsample residual $COMMON
 python train.py --model odenet --downsample residual --adjoint $COMMON
 python train.py --model odenet --downsample one-shot --adjoint $COMMON
 
+find runs_mnist -maxdepth 1 -mindepth 1 -exec python evaluate.py nfe {} --tol 0.001 \;
+find runs_mnist -maxdepth 1 -mindepth 1 -exec python evaluate.py features {} \;
+find runs_mnist -maxdepth 1 -mindepth 1 -exec python evaluate.py retrieval {} \;
+
+
 ## CIFAR-10
 COMMON="--filters 256 --dataset cifar10 --augmentation crop+jitter+flip+norm --epochs 250 --batch-size $BS --lr $LR --patience 15 --dropout $DROPOUT --wd $WD"
 
 python train.py --model resnet --downsample residual $COMMON
 python train.py --model odenet --downsample residual --adjoint $COMMON
 python train.py --model odenet --downsample one-shot --adjoint $COMMON
+
+find runs_cifar10 -maxdepth 1 -mindepth 1 -exec python evaluate.py nfe {} --tol 0.001 \;
+find runs_cifar10 -maxdepth 1 -mindepth 1 -exec python evaluate.py features {} \;
+find runs_cifar10 -maxdepth 1 -mindepth 1 -exec python evaluate.py retrieval {} \;
+
+
+
