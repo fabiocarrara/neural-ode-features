e8d3cec328c3c739b12260df612bceb5bcf7f3e3
Fabio Carrara <fabio.carrara@isti.cnr.it>
2019-07-22 18:07:00 +0200

    added attacks with CIFAR10, added stepsize

--- adversarial/attack.py
+++ adversarial/attack.py
@@ -1,16 +1,17 @@
 import argparse
 import os
+import time
+
 import foolbox
 import numpy as np
 import pandas as pd
-import time
-
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR10
 from torchvision.transforms import transforms
 from tqdm import tqdm
 
 import utils
 from expman import Experiment
+
 
 def main(args):
     exp = Experiment.from_dir(args.run, main='model')
@@ -21,29 +22,35 @@ def main(args):
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.numpy())
     ])
-    data = MNIST('data/mnist', download=True, train=False, transform=transform)
+
+    preproc = utils.PREPROC[params.dataset]
+    if params.dataset == 'mnist':
+        data = MNIST('data/mnist', download=True, train=False, transform=transform)
+    elif params.dataset == 'cifar10':
+        data = CIFAR10('data/cifar10', download=True, train=False, transform=transform)
+        preproc = map(lambda x: np.array(x).reshape((3, 1, 1)), preproc)  # expand dimensions
+        preproc = tuple(preproc)
 
     # model setup
     model = utils.load_model(exp).eval().cuda()
     if args.tol is None:
         args.tol = params.tol
 
-    if params.model == "odenet":
+    if params.model == 'odenet':
         model.odeblock.tol = args.tol
 
-    preproc = utils.PREPROC['mnist']
-    fmodel = foolbox.models.PyTorchModel(model, bounds=preproc, num_classes=10, preprocessing=preproc)
+    fmodel = foolbox.models.PyTorchModel(model, bounds=(0, 1), num_classes=10, preprocessing=preproc)
 
     # attack setup
-    if args.distance == '2':
+    if args.distance == 2:
         attack = foolbox.attacks.L2BasicIterativeAttack
         distance = foolbox.distances.MSE
-    elif args.distance == 'inf':
+    elif args.distance == float('inf'):
         attack = foolbox.attacks.LinfinityBasicIterativeAttack
         distance = foolbox.distances.Linf
-    
+
     attack = attack(fmodel, distance=distance)
-    
+
     sub_exp_root = exp.path_to('adv-attack')
     os.makedirs(sub_exp_root, exist_ok=True)
 
@@ -56,27 +63,31 @@ def main(args):
     progress = tqdm(data)
     for i, (image, label) in enumerate(progress):
         if not results.empty and i in results.sample_id.values:
-           continue
-        
+            continue
+
+        if not isinstance(label, int):
+            label = label.item()
+
         start = time.time()
-        adversarial = attack(image, label, unpack=False, binary_search=False, epsilon=args.epsilon)
+        adversarial = attack(image, label, unpack=False, binary_search=False, stepsize=args.stepsize, epsilon=args.epsilon)
+
         elapsed = time.time() - start
         result = pd.DataFrame(dict(
-            sample_id         = i,
-            label             = label,
-            elapsed_time      = elapsed,
-            distance          = adversarial.distance.value,
-            adversarial_class = adversarial.adversarial_class,
-            original_class    = adversarial.original_class,
+            sample_id=i,
+            label=label,
+            elapsed_time=elapsed,
+            distance=adversarial.distance.value,
+            adversarial_class=adversarial.adversarial_class,
+            original_class=adversarial.original_class,
         ), index=[0])
-        
+
         results = results.append(result, ignore_index=True)
         results.to_csv(results_file, index=False)
-        
-        success = (results.adversarial_class != results.original_class)
+
+        success = ~results.adversarial_class.isna()
         successes = success.sum()
         success_rate = success.mean()
-        
+
         progress.set_postfix({'success_rate': f'{success_rate:.2%} ({successes}/{len(success)})'})
 
 
@@ -85,11 +96,9 @@ if __name__ == '__main__':
     parser.add_argument('run', help='Run to attack')
     parser.add_argument('-t', '--tol', type=float, default=None, help='ODE solver tolerance')
     parser.add_argument('-a', '--attack', choices=('pgd',), default='pgd', help='Attack to perform')
-    parser.add_argument('-d', '--distance', choices=('2', 'inf'), default='inf', help='L_p distance')
-    parser.add_argument('-e', '--epsilon', type=float, default=0.3, help='epsilon')
-    
-    
-    
+    parser.add_argument('-s', '--stepsize', type=float, default=0.05, help='step size for iterative attacks')
+    parser.add_argument('-d', '--distance', type=float, choices=(2.0, float('inf')), default=float('inf'), help='L_p distance')
+    parser.add_argument('-e', '--epsilon', type=float, default=0.3, help='maximum perturbation allowed')
 
     args = parser.parse_args()
     main(args)
